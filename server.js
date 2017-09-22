68d0d4c7602ddb4cc5ab3ea100c15557ee50909f
Yora Li <yorali@Yoras-MacBook-Air.local>
2017-09-22 18:31:11 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mongojs = require("mongoose");
+var request = require('request');
+var cheerio = require('cheerio');
+
+var app = express();
+
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+var db = mongojs(databaseUrl, collections);
+db.on('error', function(error) {
+    console.log("Database Error:", error);
+});
+
+app.get("/", function(req, res) {
+    res.send("Placeholder Message");
+});
